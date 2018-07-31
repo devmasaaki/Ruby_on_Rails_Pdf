f9f196b28482410c3cb5d0724decda62439b7abe
dev92341 <jasper92341@hotmail.com>
2018-07-30 23:21:46 -0300

    Adding filepicker-rails config

--- Gemfile
+++ Gemfile
@@ -58,3 +58,5 @@ gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
 
 gem 'wicked_pdf'
 gem 'wkhtmltopdf-binary'
+
+gem 'filepicker-rails'
--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,7 @@ module CodingChallenge
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 5.2
-
+    config.filepicker_rails.api_key = ENV['FILEPICKER_API_KEY']
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration can go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded after loading
